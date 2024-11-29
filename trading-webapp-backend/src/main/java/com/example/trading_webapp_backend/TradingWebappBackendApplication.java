d55e00df5dace34dc2b1b637ce916ec2509846be
Diogo <diogoafg7@gmail.com>
2024-11-29 15:48:14 +0000

    Backend pasta remove

---
+++ trading-webapp-backend/src/main/java/com/example/trading_webapp_backend/TradingWebappBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.trading_webapp_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradingWebappBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradingWebappBackendApplication.class, args);
+	}
+
+}
---
+++ trading-webapp-backend/src/test/java/com/example/trading_webapp_backend/TradingWebappBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.trading_webapp_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradingWebappBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
