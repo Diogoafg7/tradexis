0711dd419184c329ea9f6f1b9a6f950f4e5a94cc
Diogo <diogoafg7@gmail.com>
2024-11-29 00:59:47 +0000

    Spring Inicializado Database Criada Ligação com os dois feita

---
+++ backend/trading-webapp-backend/src/main/java/com/example/trading_webapp_backend/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.example.trading_webapp_backend.repository;
+
+import com.example.trading_webapp_backend.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+    User findByUsername(String username);
+}
