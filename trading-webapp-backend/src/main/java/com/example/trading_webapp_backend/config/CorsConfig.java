44bd060b0084bc046603937d26d5041c859b172f
Wilson Miranda <intership.natixis@hotmail.com>
2024-12-02 10:26:23 +0000

    update corsConfig  remove import exemple.

--- trading-webapp-backend/src/main/java/com/example/trading_webapp_backend/config/CorsConfig.java
+++ trading-webapp-backend/src/main/java/com/example/trading_webapp_backend/config/CorsConfig.java
@@ -1,6 +1,6 @@
 package com.example.trading_webapp_backend.config;
 
-//import org.springframework.context.annotation.Bean;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
