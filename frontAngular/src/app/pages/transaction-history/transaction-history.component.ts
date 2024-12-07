e3444fde06982d00698929a7db093ee462aca4d1
MagsPeixoto <margaridapeixoto1996@gmail.com>
2024-12-07 12:18:50 +0000

    Connecting with Backend on Stock History

--- frontAngular/src/app/pages/transaction-history/transaction-history.component.ts
+++ frontAngular/src/app/pages/transaction-history/transaction-history.component.ts
@@ -1,59 +1,41 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
-import { NgClass, NgFor, NgIf } from '@angular/common';
+import { CommonModule, NgClass, NgFor, NgIf } from '@angular/common';
+import { StockServiceService } from '../../stock-service.service';
 
 @Component({
   selector: 'app-transaction-history',
-  imports: [HeaderComponent, NgFor, NgIf, NgClass],
+  imports: [HeaderComponent, NgFor, NgIf, NgClass, CommonModule],
   templateUrl: './transaction-history.component.html',
   styleUrl: './transaction-history.component.scss'
 })
+
 export class TransactionHistoryComponent {
-  // Lista de Ações
-  acoes = [
-    { 
-      nome: 'TESLA', 
-      sigla: 'TSLA', 
-      preco: '$336.09', 
-      diferenca: '+0.07%', 
-      percentual: '(0.21%)', 
-      detalhes: {
-        tipo: 'Stock',
-        status: 'Concluído',
-        nome: 'Tesla (TSLA)',
-        valorInvestido: '$1,200.00',
-        precoExecucao: '$1,200.00',
-        quantidade: '3 ações',
-        submetidaEm: '02/12/2024',
-        resultado: '$20,000'
-      }
-    },
-    { 
-      nome: 'AMAZON', 
-      sigla: 'AMZN', 
-      preco: '$3,250.10', 
-      diferenca: '-0.15%', 
-      percentual: '(1.08%)', 
-      detalhes: {
-        tipo: 'Stock',
-        status: 'Concluído',
-        nome: 'Amazon (AMZN)',
-        valorInvestido: '$2,500.00',
-        precoExecucao: '$2,450.00',
-        quantidade: '5 ações',
-        submetidaEm: '03/12/2024',
-        resultado: '$-500'
-      }
-    }
-  ];
 
-  // Variável para armazenar os detalhes da transação a ser exibido
-  detalhesTransacao: any = null;
-  // Variável para armazenar o índice da ação selecionada
-  selectedIndex: number | null = null;
+  stockHistories: any[] = [];
+  stockDetails: any | null = null;
 
-  // Função para mostrar detalhes da transação
-  mostrarDetalhes(index: number) {
-    this.detalhesTransacao = this.acoes[index].detalhes;
+  constructor(private stockHistoryService: StockServiceService) {}
+
+  ngOnInit(): void {
+    this.loadAllStockHistories();
   }
+  
+  // Carregar todos os históricos de preço
+  loadAllStockHistories(): void {
+    this.stockHistoryService.getAllStockHistories().subscribe((data) => {
+      this.stockHistories = data;
+      console.log(data, 'AllStocks');
+    });
+  }
+  
+  
+  // Obter detalhes de um único histórico
+  getStockDetails(id: number): void {
+    this.stockHistoryService.getStockHistoryById(id).subscribe((data) => {
+      this.stockDetails = data;
+      console.log(data, 'AllStocksById');
+    });
+  }
+
 }
